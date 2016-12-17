91ecd8d4ddbff69083c8970ac08acf2d7c48c1e2
Ophir LOJKINE <pere.jobs@gmail.com>
2016-12-17 17:31:18 +0300

    fix

--- expectation–maximization.js
+++ expectation–maximization.js
@@ -11,72 +11,73 @@ function multivariate_gaussian_fit(points, n_groups, epsilon) {
 n_groups = n_groups || 1;
 epsilon = epsilon || 1e-5;
 
-var k = points[0].length; // dimension of the vector-space of the input data
-
-// Random initialization parameters for our data groups
-var groups = n.rep([n_groups], 0).map(() => ({
-    sigma: n.diag(n.random([k])),
-    mu: n.random([k]),
-    weight:Math.random()
-}));
+var dim = points[0].length; // dimension of the vector-space of the input data
 
 // == Algorithm ==
 
 // Maximization phase
-function update_groups(groups, tiks) {
-  const npoints = points.length;
-  return groups.map(function (group, k) {
+function compute_groups(tiks) {
+  const sum = n.sum(tiks);
+  return tiks.map(function (tik) {
     // We use Number.EPSILON instead of zero to avoid converging to a null solution
     // If a group is currently far-off, we don't want it's weight to become 0, or its sigma to become infinite
-    const tiksum = tiks.reduce((p,tik) => p + tik[k], n.epsilon);
-    return tiks.reduce(function (newgroup, tik, i) {
-      const tickval = tik[k], curpoint = points[i];
-      newgroup.weight += tickval / npoints;
-      n.addeq(newgroup.mu, n.mul(tickval/tiksum, curpoint));
-      const diff = [n.sub(curpoint, group.mu)];
-      n.addeq(newgroup.sigma,
-              n.mul(tickval / tiksum,
-                    n.dot(n.transpose(diff), diff)));
-      return newgroup;
-     }, {
-          weight:n.epsilon,
-          mu: n.mul(group.mu, 0),
-          sigma: n.muleq(n.identity(group.mu.length), n.epsilon/100)
-     });
+    const tiksum = n.sum(tik);
+    const weight = tiksum / sum;
+    const mu = n.transpose(n.div(points,tiksum))
+                  .map(xs => n.sum(n.mul(xs, tik)));
+    var sigma =n.diag(n.rep([dim], n.epsilon));
+    for(var i=0; i<points.length; i++) {
+        var point = points[i];
+        var diff = [n.sub(point, mu)];
+        n.addeq(sigma, n.mul(tik[i] / tiksum,
+                             n.dot(n.transpose(diff), diff)));
+    }
+    return {
+        weight: weight,
+        sigma:sigma,
+        mu:mu
+    }
   });
 }
 
 // Estimation phase
 function tiks(groups) {
-  // Precompute the density functions
-  const density = groups.map(gaussian);
-  // Return for each point, the probability of it being in each group
-  return points.map(function(point) {
-    // coeff = 1 / sum(density[i](point))
-    const coeff = 1 / n.sum(density.map(f => f(point)));
-    return groups.map((group, i) => coeff * density[i](point));
-  });
+  var res = [];
+  for (var g=0; g<n_groups; g++) {
+    var line = [];
+    var group = groups[g];
+    // Precompute the density functions
+    var density_fun = gaussian(group);
+    for(var p=0; p<points.length; p++) {
+      var point = points[p];
+      line.push(group.weight * density_fun(point));
+    }
+    res.push(line);
+  }
+  for(var p=0; p<points.length; p++) {
+    var sum = 0;
+    for(var g=0; g<n_groups; g++) {
+      sum += res[g][p];
+    }
+    for(var g=0; g<n_groups; g++) {
+      res[g][p] /= sum;
+    }
+  }
+  return res;
 }
 
 // Itereate, alterning estimation and maximization
-var ts = null;
-for (var i=0; i<300; i++) {
+var ts = n.random([n_groups, points.length]);
+for (var i=0; i<50; i++) {
+  console.log(ts);
+  var groups = compute_groups(ts);
   var oldts = ts, ts = tiks(groups);
-  if (oldts) {
-    var delta = n.sum(n.abs(n.sub(ts, oldts)));
-    if (delta <= epsilon) break;
-  }
-  groups = update_groups(groups, ts);
+
+  var delta = n.norminf(n.sub(ts, oldts));
+  if (delta <= epsilon) break;
 }
 
 return groups;
 }
 
-// Our data points
-var points = [];
-for(var i=0; i<500; i++)
-  points.push(Math.random() > .5 ? [Math.random(), Math.random()]
-                                 : [-1 + 2*Math.random(), 2*Math.random()]);
-
-var groups = multivariate_gaussian_fit(points, 2, 1e-5);
-console.log(JSON.stringify(groups, null, '  '));
+module.exports = multivariate_gaussian_fit;
