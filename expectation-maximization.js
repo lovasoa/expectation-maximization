0c8fc04d5a19eb0141f561a93f6bb1f4a075bbe6
Ophir LOJKINE <pere.jobs@gmail.com>
2016-12-18 01:17:08 +0300

    Beautify code

--- expectation-maximization.js
+++ expectation-maximization.js
@@ -1,82 +1,84 @@
-// Expectation–maximization (EM) algorithm, implemented in Javascript (ES6) for floating-point variables
+"use strict";
+// Expectation–maximization (EM) algorithm, implemented in js
 // Ophir LOJKINE, december 2016
 // Published under WTFPL
-//  == User parameters ==
 
+//  == User parameters ==
 var n = require("numeric");
 var gaussian = require('multivariate-gaussian');
 
 function multivariate_gaussian_fit(points, n_groups, epsilon) {
+  n_groups = n_groups || 1;
+  epsilon = epsilon || n.epsilon;
 
-n_groups = n_groups || 1;
-epsilon = epsilon || n.epsilon;
+  // dimension of the vector-space of the input data
+  var dim = points[0].length;
 
-var dim = points[0].length; // dimension of the vector-space of the input data
-
-// == Algorithm ==
-
-// Maximization phase
-function compute_groups(tiks) {
-  const sum = n.sum(tiks);
-  return tiks.map(function (tik) {
-    // We use Number.EPSILON instead of zero to avoid converging to a null solution
-    // If a group is currently far-off, we don't want it's weight to become 0, or its sigma to become infinite
-    const tiksum = n.sum(tik);
-    const weight = tiksum / sum;
-    const mu = n.transpose(n.div(points,tiksum))
-                  .map(xs => n.sum(n.mul(xs, tik)));
-    var sigma =n.diag(n.rep([dim], n.epsilon));
-    for(var i=0; i<points.length; i++) {
+  // == Algorithm ==
+  // Maximization phase
+  function compute_groups(tiks) {
+    var sum = n.sum(tiks);
+    return tiks.map(function (tik) {
+      var tiksum = n.sum(tik);
+      var weight = tiksum / sum;
+      var mu = n.transpose(n.div(points, tiksum)).map(function (
+        xs) {
+        return n.sum(n.mul(xs, tik));
+      });
+      var sigma = n.diag(n.rep([dim], n.epsilon));
+      for (var i = 0; i < points.length; i++) {
         var point = points[i];
         var diff = [n.sub(point, mu)];
-        n.addeq(sigma, n.mul(tik[i] / tiksum,
-                             n.dot(n.transpose(diff), diff)));
-    }
-    return {
+        var coeff = tik[i] / tiksum;
+        var diffdiff = n.dot(n.transpose(diff), diff);
+        n.addeq(sigma, n.mul(coeff, diffdiff));
+      }
+      return {
         weight: weight,
-        sigma:sigma,
-        mu:mu
-    }
-  });
-}
-
-// Estimation phase
-function tiks(groups) {
-  var res = [];
-  for (var g=0; g<n_groups; g++) {
-    var line = [];
-    var group = groups[g];
-    // Precompute the density functions
-    var density_fun = gaussian(group);
-    for(var p=0; p<points.length; p++) {
-      var point = points[p];
-      line.push(group.weight * density_fun(point));
-    }
-    res.push(line);
+        sigma: sigma,
+        mu: mu
+      };
+    });
   }
-  for(var p=0; p<points.length; p++) {
-    var sum = 0;
-    for(var g=0; g<n_groups; g++) {
-      sum += res[g][p];
+
+  // == Estimation phase ==
+  function tiks(groups) {
+    var res = [];
+    for (var g = 0; g < n_groups; g++) {
+      var line = [];
+      var group = groups[g];
+      // Precompute the density functions
+      var density_fun = gaussian(group);
+      for (var p = 0; p < points.length; p++) {
+        var point = points[p];
+        line.push(group.weight * density_fun(point));
+      }
+      res.push(line);
     }
-    for(var g=0; g<n_groups; g++) {
-      res[g][p] /= sum;
+    for (var p = 0; p < points.length; p++) {
+      var sum = 0;
+      for (var g = 0; g < n_groups; g++) {
+        sum += res[g][p];
+      }
+      for (var g = 0; g < n_groups; g++) {
+        res[g][p] /= sum;
+      }
     }
+    return res;
   }
-  return res;
-}
 
-// Itereate, alterning estimation and maximization
-var ts = n.random([n_groups, points.length]);
-for (var i=0; i<1000; i++) {
-  var groups = compute_groups(ts);
-  var oldts = ts, ts = tiks(groups);
-
-  var delta = n.norminf(n.sub(ts, oldts));
-  if (delta <= epsilon) break;
-}
-
-return groups;
+  // == Main loop ==
+  // Attribute points to groups randomly
+  var ts = n.random([n_groups, points.length]);
+  // Itereate, alterning estimation and maximization
+  for (var i = 0; i < 1000; i++) {
+    var groups = compute_groups(ts);
+    var oldts = ts;
+    ts = tiks(groups);
+    var delta = n.norminf(n.sub(ts, oldts));
+    if (delta <= epsilon) break;
+  }
+  return groups;
 }
 
 module.exports = multivariate_gaussian_fit;
--- test/index.js
+++ test/index.js
@@ -4,25 +4,31 @@ var assert = require('assert');
 
 function assertNear(a, b, epsilon) {
   epsilon = epsilon || .1;
-  assert(n.norm2(n.sub(a,b)) < epsilon,
-         JSON.stringify(a) + ' near ' + JSON.stringify(b));
+  assert(n.norm2(n.sub(a, b)) < epsilon,
+    JSON.stringify(a) + ' near ' + JSON.stringify(b));
 }
 
 // Our data points
 // We create two groups: one centered on (5,5), the other on(0,1)
 var points = [];
-var groups = [{weight: .3, mu: [5,5]},{weight: .7, mu: [0,1]}];
-for(var i=0; i<10000; i++) {
+var groups = [{
+  weight: .3,
+  mu: [5, 5]
+}, {
+  weight: .7,
+  mu: [0, 1]
+}];
+for (var i = 0; i < 10000; i++) {
   var group = Math.random() < groups[0].weight ? 0 : 1;
   var mu = groups[group].mu;
   var r = n.clone(mu);
   var N = 10;
-  for(var k=0; k<N; k++) n.addeq(r, n.sub(n.random([2]),[.5,.5]));
+  for (var k = 0; k < N; k++) n.addeq(r, n.sub(n.random([2]), [.5, .5]));
   points.push(r);
 }
 
 var result = multivariate_gaussian_fit(points, 2);
-result.sort((a,b) => a.weight - b.weight);
+result.sort((a, b) => a.weight - b.weight);
 console.log(result)
 
 assertNear(result[0].weight, groups[0].weight);
